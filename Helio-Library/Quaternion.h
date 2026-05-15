b3f0a506b616c57203a1ae3a47dcb2464f62ae73
sylistine <aaron.d.hull@gmail.com>
2026-05-15 23:44:20 +0900

    Inlined quaternion implementation.

--- Helio-Library/Quaternion.h
+++ Helio-Library/Quaternion.h
@@ -2,6 +2,7 @@
 
 /// <summary>
 /// Quaternions are useful for encoding rotations.
+/// TODO: In larger projects, it might be wise to remove +, -, and / to avoid confusion.
 /// </summary>
 struct Quaternion
 {
@@ -24,99 +25,10 @@ public:
     constexpr Quaternion operator-(const Quaternion& rhs) const;
     constexpr Quaternion operator*(const Quaternion& rhs) const;
     constexpr Quaternion operator/(const Quaternion& rhs) const;
+    
+    // TODO: operator==
+    // TODO: operator!=
+    // TODO: Quaternion AngleAxis constructor
 };
 
-inline constexpr Quaternion::Quaternion()
-    : a(0.0f), b(0.0f), c(0.0f), d(0.0f)
-{}
-
-inline constexpr Quaternion::Quaternion(float a, float b, float c, float d)
-    : a(a), b(b), c(c), d(d)
-{}
-
-inline constexpr Quaternion& Quaternion::operator=(const Quaternion& rhs)
-{
-    if (&rhs == this) {
-        return *this;
-    }
-
-    a = rhs.a;
-    b = rhs.b;
-    c = rhs.c;
-    d = rhs.d;
-    return *this;
-}
-
-inline constexpr Quaternion& Quaternion::operator+=(const Quaternion& rhs)
-{
-    a += rhs.a;
-    b += rhs.b;
-    c += rhs.c;
-    d += rhs.d;
-    return *this;
-}
-
-inline constexpr Quaternion& Quaternion::operator-=(const Quaternion& rhs)
-{
-    a -= rhs.a;
-    b -= rhs.b;
-    c -= rhs.c;
-    d -= rhs.d;
-    return *this;
-}
-
-inline constexpr Quaternion& Quaternion::operator*=(const Quaternion& rhs)
-{
-    float oa = a*rhs.a - b*rhs.b - c*rhs.c - d*rhs.d;
-    float ob = a*rhs.b + b*rhs.a + c*rhs.d - d*rhs.c;
-    float oc = a*rhs.c - b*rhs.d + c*rhs.a + d*rhs.b;
-    float od = a*rhs.d + b*rhs.c - c*rhs.b + d*rhs.a;
-    a = oa;
-    b = ob;
-    c = oc;
-    d = od;
-    return *this;
-}
-
-inline constexpr Quaternion& Quaternion::operator/=(const Quaternion& rhs)
-{
-    float den = rhs.a + rhs.b + rhs.c + rhs.d;
-    float oa = (a*rhs.a + b*rhs.b + c*rhs.c + d*rhs.d) / den;
-    float ob = (a*rhs.b - b*rhs.a - c*rhs.d + d*rhs.c) / den;
-    float oc = (a*rhs.c + b*rhs.d - c*rhs.a - d*rhs.b) / den;
-    float od = (a*rhs.d - b*rhs.c + c*rhs.b - d*rhs.a) / den;
-    a = oa;
-    b = ob;
-    c = oc;
-    d = od;
-    return *this;
-}
-
-inline constexpr Quaternion Quaternion::operator+(const Quaternion& rhs) const
-{
-    return Quaternion(a + rhs.a, b + rhs.b, c + rhs.c, d + rhs.d);
-}
-
-inline constexpr Quaternion Quaternion::operator-(const Quaternion& rhs) const
-{
-    return Quaternion(a - rhs.a, b - rhs.b, c - rhs.c, d - rhs.d);
-}
-
-inline constexpr Quaternion Quaternion::operator*(const Quaternion& rhs) const
-{
-    float oa = a * rhs.a - b * rhs.b - c * rhs.c - d * rhs.d;
-    float ob = a * rhs.b + b * rhs.a + c * rhs.d - d * rhs.c;
-    float oc = a * rhs.c - b * rhs.d + c * rhs.a + d * rhs.b;
-    float od = a * rhs.d + b * rhs.c - c * rhs.b + d * rhs.a;
-    return Quaternion(oa, ob, oc, od);
-}
-
-inline constexpr Quaternion Quaternion::operator/(const Quaternion& rhs) const
-{
-    float den = rhs.a + rhs.b + rhs.c + rhs.d;
-    float oa = (a * rhs.a + b * rhs.b + c * rhs.c + d * rhs.d) / den;
-    float ob = (a * rhs.b - b * rhs.a - c * rhs.d + d * rhs.c) / den;
-    float oc = (a * rhs.c + b * rhs.d - c * rhs.a - d * rhs.b) / den;
-    float od = (a * rhs.d - b * rhs.c + c * rhs.b - d * rhs.a) / den;
-    return Quaternion(oa, ob, oc, od);
-}
+#include "Quaternion.inl"
